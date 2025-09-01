bdfce263858fb42db7854dec4a4f7dfeaa07fd03
sik2 <bickwang123@gmail.com>
2025-09-01 21:10:45 +0900

    refactor: forPrintPokemons & feat: useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 function App() {
   const [pocketmons, setPocketmons] = useState([]);
@@ -6,6 +6,9 @@ function App() {
   const [loding, setLoading] = useState(true);
   const limit = 30;
   const totalCount = 1302;
+
+  // forPrintPokemons 실행 되는지 테스트하기 위한 변수
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
@@ -18,10 +21,30 @@ function App() {
 
   const getNumberFromUrl = (url) => {
     const urlItems = url.split("/");
-    console.log(urlItems);
-
     return parseInt(urlItems[6]);
   };
+
+  // 실행되는지를 테스트 하는 변수
+  let i = 0;
+  // useMemozation 설정
+  const forPrintPokemons = useMemo(
+    () =>
+      pocketmons.map((pocketmon) => {
+        // 실행되는지를 테스트
+        console.log("실행됨", ++i);
+
+        const imgNum = getNumberFromUrl(pocketmon.url);
+        const imgUrl = `https://cdn.jsdelivr.net/gh/PokeAPI/sprites/sprites/pokemon/${imgNum}.png`;
+
+        return {
+          imgNum,
+          imgUrl,
+          ...pocketmon,
+        };
+      }),
+    // pocketmons 이 변경될때만 실행되도록 deps(의존배열) 추가
+    [pocketmons]
+  );
 
   const showPrev = () => {
     setOffset(offset - limit);
@@ -37,11 +60,13 @@ function App() {
 
   return (
     <>
+      {/* 카운트가 변경되면 forPrintPokemons 가 계속 실행되지만 useMemo를 도입하면 필요할때만 실행되록 할 수 있다.*/}
+      count: <button onClick={() => setCount(count + 1)}>{count}</button>
       <h1>포켓몬 도감</h1>
       {offset > 0 && <button onClick={showPrev}>이전</button>}
       {totalCount > offset + limit && <button onClick={showNext}>다음</button>}
       <ul>
-        {pocketmons.map((pocketmon, index) => (
+        {forPrintPokemons.map((pocketmon, index) => (
           <li
             key={index}
             style={{
@@ -50,12 +75,8 @@ function App() {
               borderbottom: "1px solid black",
             }}
           >
-            <img
-              src={`https://cdn.jsdelivr.net/gh/PokeAPI/sprites/sprites/pokemon/${getNumberFromUrl(
-                pocketmon.url
-              )}.png`}
-              alt="pocketmon"
-            />
+            <span>{pocketmon.imgNum}</span>
+            <img src={pocketmon.imgUrl} alt="pocketmon" />
             {pocketmon.name}
           </li>
         ))}
