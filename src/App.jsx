e910d8fc18e7f49bf66e2159573e36b4ccc22134
sik2 <bickwang123@gmail.com>
2025-09-01 20:30:19 +0900

    feat:showPrev, showNext, loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,37 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [pocketmons, setPocketmons] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [loding, setLoading] = useState(true);
+  const limit = 30;
+  const totalCount = 1302;
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
       .then((res) => res.json())
-      .then((res) => setPocketmons(res.results));
-  });
+      .then((res) => {
+        setPocketmons(res.results);
+        setLoading(false);
+      });
+  }, [offset]);
+
+  const showPrev = () => {
+    setOffset(offset - limit);
+  };
+
+  const showNext = () => {
+    setOffset(offset + limit);
+  };
+
+  if (loding) {
+    return <>로딩중...</>;
+  }
 
   return (
     <>
       <h1>포켓몬 도감</h1>
+      {offset > 0 && <button onClick={showPrev}>이전</button>}
+      {totalCount > offset + limit && <button onClick={showNext}>다음</button>}
       <ul>
         {pocketmons.map((pocketmon, index) => (
           <li
