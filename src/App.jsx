2328e473e93971f2aae512a8876bffdca3070987
sik2 <bickwang123@gmail.com>
2025-09-01 19:59:42 +0900

    feat: api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>Hello World</>;
+  const [pocketmons, setPocketmons] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((res) => setPocketmons(res.results));
+  });
+
+  return (
+    <>
+      <h1>포켓몬 도감</h1>
+      <ul>
+        {pocketmons.map((pocketmon, index) => (
+          <li key={index}>{pocketmon.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
